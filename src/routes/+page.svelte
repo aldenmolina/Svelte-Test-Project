09a72a39fe18a839f41bdc85b4ad7e76c7debaa7
Alden Molina <ajmolina092@gmail.com>
2023-01-20 16:49:04 -0500

    Testing functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    let count = 0;
+
+    function handleIncrement() {
+        count += 1;
+    }
+</script>
+
+<h1>Number: {count}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={handleIncrement}>Increment</button>
